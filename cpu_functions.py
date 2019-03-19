0f36c427a87d84383f3f656231fe8c9dbc4770a3
Henry V. Gilbert <henrygilbert609@gmail.com>
2019-03-19 01:00:00 -0400

    Add files via upload

--- cpu_functions.py
+++ cpu_functions.py
@@ -1,5 +1,15 @@
 from tkinter import Tk, Label, Entry, Button, Text
 
+
+def main():
+    root = Tk()
+    program = Program(keys)
+    window = GUI(root, program)
+    window.root.mainloop()
+    return None
+
+
+# Keys denote the program to be executed.
 keys = ["0100",
         "1010",
         "1010",
@@ -15,141 +25,174 @@ keys = ["0100",
         "1010",
         "1010",
         "1010",
-        "1010", ]
-
-
-def main():
-    root = Tk()
-    master = Program(keys)
-    GUI(root, master)
-    root.geometry("450x760")
-    root.mainloop()
+        "1010",]
 
 
 class Program:
+    """ Purely for the functionality of the program. Creates an instance
+    of the program that contains all functions to be carried out. Instance
+    accepts RAM input """
+
     instructions_dictionary = {
-        "1010": "LDA",
-        "2030": "LDB",
-        "3043": "LDC",
-        "4098": "LDD",
-        "0005": "ADD",
-        "0006": "SUB",
-        "0003": "DET",
-        "0007": "JMP",
-        "0008": "GRT",
-        "0009": "LSS",
-        "0010": "STR",
-        "0011": "PSH",
-        "0012": "POP",
-        "0013": "NOP",
+        # All instructions work with 4 digit op codes. The first two digits specify
+        # the command. The last two specify the memory address. can be pass.
+
+        "10": "LDA",
+        "20": "LDB",
+        "30": "LDC",
+        "40": "LDD",
+        "00": "ADD",
+        "00": "SUB",
+        "00": "DET",
+        "00": "JMP",
+        "00": "GRT",
+        "00": "LSS",
+        "00": "STR",
+        "00": "PSH",
+        "00": "POP",
+        "00": "NOP",
     }
 
-    # Programmable Section
+    function_dictionary = {
+        "LDA": LDA(),
+        "LDB": LDB(),
+        "LDC": LDC(),
+        "LDD": LDD(),
+        "ADD": ADD(),
+        "SUB": SUB()
+    }
+
+
     def __init__(self, RAM):
         self.RAM = RAM
-        self.REG = [0, 0, 0, 0]
+        self.REG = [0, 0, 0, 0]    # Corresponds to registers A, B, C, D.
         self.instruction_register = ''
         self.counter = 0
+        self.inst_decoded = ' '
 
-    def initialize(self):
-        return 0
 
     def fetch(self):
+        self.instruction_register = self.RAM[self.counter]
         print("Fetch")
-        return None
+
+
 
     def decode(self):
-        # Works with a four digit op code: seperated into two two-digit commands
+        self.inst_decoded = self.instruction_register[0:2]   # loads first two digits
         print("Decode")
 
+
     def execute(self):
-        print("Execute")
-        # Calls the appropriate function to execute from
-        # the function dictionary. Stores the value in memory.
+
+
         self.counter += 1
-        print(self.counter)
-        return
+        print("Execute")
+
+
 
     def LDA(self):
-        self.REG[0] = self.instruction_register
+        self.REG[0] = int(self.instruction_register)
 
     def LDB(self):
-        self.REG[1] = self.instruction_register
+        self.REG[1] = int(self.instruction_register)
 
     def LDC(self):
-        self.REG[2] = self.instruction_register
+        self.REG[2] = int(self.instruction_register)
 
     def LDD(self):
-        self.REG[3] = self.instruction_register
+        self.REG[3] = int(self.instruction_register)
 
     def ADD(self):
         sum = self.REG[0] = self.REG[1]
+        return sum
+
+    def SUB(self):
+        diff = self.REG[2] - self.REG[3]
+        return diff
 
     pass
 
 
 class GUI:
-    """ Pass the root/master variable into  master/root = Tk(), and program = the
-        instance created from the program class. """
+    """ Accepts two inputs. The first is the app/root file from Tk(). The
+    second input is the program input that holds all relevant methods and
+    instance variables. """
 
-    def __init__(self, app, program):
+
+    def __init__(self, root, program):
         self.program = program  # Gives the GUI the process instance methods.
+        self.root = root
+        self.root.geometry("400x800")
 
         # Instruction register section.
-        self.instruction_register = Entry(app, width=5, state='readonly')
+        self.instruction_register = Entry(self.root, width=5)
         self.instruction_register.grid(row=0, column=1)
-        self.instruction_register_label = Label(app, text="INSTRUCTION REGISTER").grid(row=0, column=0)
+        self.instruction_register_label = Label(self.root, text="INSTRUCTION REGISTER").grid(row=0, column=0)
 
         # Program counter section.
-        self.program_counter = Entry(app, width=5, state='readonly')
+        self.program_counter = Entry(self.root, width=5)
         self.program_counter.grid(row=1, column=1)
-        self.program_counter_label = Label(app, text="PROGRAM COUNTER").grid(row=1, column=0)
+        self.program_counter_label = Label(self.root, text="PROGRAM COUNTER").grid(row=1, column=0)
 
-        # ========= Fetch ==========
-        self.fetch_button = Button(app, text='FETCH')
+        # =================== Fetch ====================
+        self.fetch_button = Button(self.root, text='FETCH', command=self.gui_fetch)
         self.fetch_button.grid(row=2, column=0, sticky="")
 
-        # ========= Decode ==========
-        self.decode_button = Button(app, text='DECODE', command=GUI.sup)
+        # =================== Decode ====================
+        self.decode_button = Button(self.root, text='DECODE', command=self.gui_decode)
         self.decode_button.grid(row=2, column=0, sticky="E")
 
-        # ========= Execute =========
-        self.execute_button = Button(app, text='EXECUTE')
+        # =================== Execute ===================
+        self.execute_button = Button(self.root, text='EXECUTE')
         self.execute_button.grid(row=2, column=1)
 
-        # ========= Initialize =======
-        self.initialize_button = Button(app, text='INITIALIZE')
+        # =================== Initialize =================
+        self.initialize_button = Button(self.root, text='INITIALIZE')
         self.initialize_button.grid(row=2, column=0, sticky="W")
 
         # RAM Section
-        self.RAM_Label = Label(app, text="RAM").grid(row=0, column=6, sticky='')
-        self.R1 = Entry(app, width=8).grid(row=1, column=6)
-        self.R2 = Entry(app, width=8).grid(row=2, column=6)
-        self.R3 = Entry(app, width=8).grid(row=3, column=6)
-        self.R4 = Entry(app, width=8).grid(row=4, column=6)
-        self.R5 = Entry(app, width=8).grid(row=5, column=6)
-        self.R6 = Entry(app, width=8).grid(row=6, column=6)
-        self.R7 = Entry(app, width=8).grid(row=7, column=6)
-        self.R8 = Entry(app, width=8).grid(row=8, column=6)
-        self.R9 = Entry(app, width=8).grid(row=9, column=6)
-        self.R10 = Entry(app, width=8).grid(row=10, column=6)
-        self.R11 = Entry(app, width=8).grid(row=11, column=6)
-        self.R12 = Entry(app, width=8).grid(row=12, column=6)
-        self.R13 = Entry(app, width=8).grid(row=13, column=6)
-        self.R14 = Entry(app, width=8).grid(row=14, column=6)
-        self.R15 = Entry(app, width=8).grid(row=15, column=6)
-        self.R16 = Entry(app, width=8).grid(row=16, column=6)
+        self.RAM_Label = Label(self.root,text="RAM").grid(row=0, column=6, sticky='')
+        self.R1 = Entry(self.root, width=8).grid(row=1, column=6)
+        self.R2 = Entry(self.root, width=8).grid(row=2, column=6)
+        self.R3 = Entry(self.root, width=8).grid(row=3, column=6)
+        self.R4 = Entry(self.root, width=8).grid(row=4, column=6)
+        self.R5 = Entry(self.root, width=8).grid(row=5, column=6)
+        self.R6 = Entry(self.root, width=8).grid(row=6, column=6)
+        self.R7 = Entry(self.root, width=8).grid(row=7, column=6)
+        self.R8 = Entry(self.root, width=8).grid(row=8, column=6)
+        self.R9 = Entry(self.root, width=8).grid(row=9, column=6)
+        self.R10 = Entry(self.root, width=8).grid(row=10, column=6)
+        self.R11 = Entry(self.root, width=8).grid(row=11, column=6)
+        self.R12 = Entry(self.root, width=8).grid(row=12, column=6)
+        self.R13 = Entry(self.root, width=8).grid(row=13, column=6)
+        self.R14 = Entry(self.root, width=8).grid(row=14, column=6)
+        self.R15 = Entry(self.root, width=8).grid(row=15, column=6)
+        self.R16 = Entry(self.root, width=8).grid(row=16, column=6)
 
-        self.terminal_label = Label(app, text='CMD', font='Hevectila 12 bold').grid(row=3, column=0)
-        self.terminal = Text(app, width=28, height=20, bd=2, relief='sunken')
+        self.terminal_label = Label(self.root, text='CMD', font='Hevectila 12 bold').grid(row=3, column=0)
+        self.terminal = Text(self.root, width=28, height=20, bd=2, relief='sunken')
         self.terminal.grid(row=4, rowspan=10, column = 0, sticky="")
 
 
     # Make the GUI update with static methods and run the Process commands at the same time.
 
-    @staticmethod
-    def sup():
-        print('sup')
+    def gui_fetch(self):
+        self.program.fetch()
+        # value = self.program.RAM[self.program.counter]
+        self.instruction_register.insert(0, self.program.RAM[self.program.counter])
+        return None
+
+
+    def gui_decode(self):
+        self.program.decode()
+        return None
+
+
+    def gui_execute(self):
+        return None
+
+
+    pass
 
 if __name__ == "__main__":
     main()
